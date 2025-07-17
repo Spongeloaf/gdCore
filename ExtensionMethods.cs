81f32db8737692eb2bb863b8ec486a3a89283bfd
Spongeloaf <peter.v@live.ca>
2025-07-16 23:55:09 -0400

    Add spatial and extension methods

--- ExtensionMethods.cs
+++ ExtensionMethods.cs
@@ -1,9 +1,10 @@
 ﻿
 using System.Text;
+using Godot;
 
 namespace GdCore;
 
-public static class ExtensionMethods
+public static class ExtensionMethodsEnum
 {
     // Formats an enum value like "myEnum" like "My Enum"
     public static string ToPrettyString(this Enum e)
@@ -25,7 +26,52 @@ public static class ExtensionMethods
 
             newResult.Append(c);
         }
+
         return newResult.ToString();
     }
+}
 
+public static class ExtensionMethodsRect
+{
+    // Godots 2d coord space is Y+ == down.
+    
+    public static Vector2I BottomLeft(this Rect2I rect)
+    {
+        return new Vector2I(rect.Position.X, rect.End.Y);
+    }
+
+    public static Vector2I BottomRight(this Rect2I rect)
+    {
+        return rect.End;
+    }
+
+    public static Vector2I TopLeft(this Rect2I rect)
+    {
+        return rect.Position;
+    }
+
+    public static Vector2I TopRight(this Rect2I rect)
+    {
+        return new Vector2I(rect.End.X, rect.Position.Y);
+    }
+
+    public static Vector2 BottomLeft(this Rect2 rect)
+    {
+        return new Vector2(rect.Position.X, rect.End.Y);
+    }
+
+    public static Vector2 BottomRight(this Rect2 rect)
+    {
+        return rect.End;
+    }
+
+    public static Vector2 TopLeft(this Rect2 rect)
+    {
+        return rect.Position;
+    }
+
+    public static Vector2 TopRight(this Rect2 rect)
+    {
+        return new Vector2(rect.End.X, rect.Position.Y);
+    }
 }
---
+++ Spatial.cs
@@ -0,0 +1,61 @@
+﻿
+public static class Spatial
+{
+    public static Rect2 GetGlobalBoundingBox(Node2D node)
+    {
+        Rect2? boundingBox = null;
+
+        void Traverse(Node current)
+        {
+            if (current is CanvasItem canvasItem && canvasItem.Visible)
+            {
+                // Check if it has a visual rect (like a Sprite2D or Control)
+                Rect2? rect = GetItemBounds(canvasItem);
+
+                if (rect != null)
+                {
+                    if (boundingBox == null)
+                        boundingBox = rect;
+                    else
+                        boundingBox = boundingBox.Value.Merge(rect.Value);
+                }
+            }
+
+            foreach (Node child in current.GetChildren())
+            {
+                Traverse(child);
+            }
+        }
+
+        Traverse(node);
+
+        return boundingBox ?? new Rect2(node.GlobalPosition, Vector2.Zero);
+    }
+
+    public static Rect2? GetItemBounds(CanvasItem item)
+    {
+        switch (item)
+        {
+            case Sprite2D sprite when sprite.Texture != null:
+                var textureSize = sprite.Texture.GetSize() * sprite.Scale;
+                var topLeft = sprite.GetGlobalTransformWithCanvas().Origin - textureSize * sprite.GetRect().GetCenter();
+                return new Rect2(topLeft, textureSize);
+
+            case Control control:
+                var globalPos = control.GetGlobalTransformWithCanvas().Origin;
+                return new Rect2(globalPos, control.Size);
+
+            case CollisionShape2D collider when collider.Shape is RectangleShape2D rect:
+                var extents = rect.Size * 0.5f;
+                var center = collider.GetGlobalTransformWithCanvas().Origin;
+                return new Rect2(center - extents, rect.Size);
+
+            case TileMapLayer tileMapLayer:
+                Vector2I sizeInPixels = tileMapLayer.GetUsedRect().End * tileMapLayer.TileSet.TileSize;
+                return new Rect2(tileMapLayer.GlobalPosition, sizeInPixels);
+        }
+
+        return null;
+    }
+
+}
