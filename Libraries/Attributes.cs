72179f1859faf75fcd468b52d9661a20f69e8005
Spongeloaf <peter.v@live.ca>
2025-11-25 23:35:42 -0500

    Update doc strings and other stuff

--- Libraries/Attributes.cs
+++ Libraries/Attributes.cs
@@ -1,11 +1,19 @@
 ﻿namespace GdCore.Libraries;
 
+/// <summary>
+/// When calling <see cref="NodeExtensionMethods.TrySetupTaggedNodes_Throws"/>,
+/// if a matching node is not found in the scene tree, an exception will be thrown.
+/// </summary>
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
 public class CriticalNodeAttribute(string nodeName) : Attribute
 {
     public readonly string NodeName = nodeName;
 }
 
+/// <summary>
+/// When calling <see cref="NodeExtensionMethods.TrySetupTaggedNodes_Throws"/>,
+/// if a matching node exists in the tree, it will be stored in the tagged member.
+/// </summary>
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
 public class OptionalNodeAttribute(string nodeName) : Attribute
 {
@@ -13,7 +21,8 @@ public class OptionalNodeAttribute(string nodeName) : Attribute
 }
 
 /// <summary>
-/// Tags a node for auto-generation. A default node will be constructed and added as a child of the
+/// When calling <see cref="NodeExtensionMethods.TrySetupTaggedNodes_Throws"/>,
+/// a default node will be constructed and added as a child of the
 /// object that owns this field/property. Will use the field/property name as the node name.
 /// </summary>
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
