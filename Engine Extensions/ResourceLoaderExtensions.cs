2991996652792cf59e59490d89ddfa74b4ab0f8d
Spongeloaf <peter.v@live.ca>
2025-12-28 12:18:50 -0500

    Enhance resourceloader utils

--- Engine Extensions/ResourceLoaderExtensions.cs
+++ Engine Extensions/ResourceLoaderExtensions.cs
@@ -4,7 +4,7 @@ namespace GdCore.Services;
 
 public static class ResourceUtils
 {
-    public static T LoadResourceOrThrow<T>(ResPath path) where T : class
+    public static T LoadResource_Throws<T>(ResPath path) where T : class
     {
         T? res = ResourceLoader.Load<T>(path.ToString());
         if (res is null)
@@ -29,5 +29,21 @@ public static class ResourceUtils
             throw new MissingResourceException(path);
 
         return scn.Instantiate<T>();
+    }
+
+    /// <summary>
+    /// Loads a scene and returns its root node. Also adds the scene as a child of the given node.
+    /// Throws if scene cannot be loaded or root node differs from T
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    /// <exception cref="MissingResourceException"></exception>
+    /// <exception cref="InvalidCastException"></exception>
+    public static T LoadSceneAsChild_Throws<T>(ResPath path, Node parent) where T : Node
+    {
+        T node = LoadScene_Throws<T>(path);
+        parent.AddChild(node);
+        return node;
     }
 }
